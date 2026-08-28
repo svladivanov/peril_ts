fd997ed47a24f46de44f1053f3e30c612b52a19a
Vlad Ivanov <vlad.ivanov@hotmail.com>
2026-08-28 09:27:28 +0200

    server connection to RabbitMQ

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,23 @@
+import amqp from 'amqplib'
+
 async function main() {
-  console.log('Starting Peril server...')
+  const rabbitConnString = 'amqp://guest:guest@localhost:5672/'
+  const conn = await amqp.connect(rabbitConnString)
+  console.log('Peril game server connected to RabbitMQ!')
+
+  const signals = ['SIGINT', 'SIGTERM']
+  signals.forEach((signal) => {
+    process.on(signal, async () => {
+      try {
+        await conn.close()
+        console.log('RabbitMQ connection closed.')
+      } catch (err) {
+        console.error('Error closing RabbitMQ connection:', err)
+      } finally {
+        process.exit(0)
+      }
+    })
+  })
 }
 
 main().catch((err) => {
