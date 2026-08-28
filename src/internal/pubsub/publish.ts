4de5f0558190494262055bc4da64190236da9d91
Vlad Ivanov <vlad.ivanov@hotmail.com>
2026-08-28 12:29:33 +0200

    exchanges and queues

---
+++ src/internal/pubsub/publish.ts
@@ -0,0 +1,26 @@
+import type { ConfirmChannel } from 'amqplib'
+
+export const publishJSON = async <T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> => {
+  const content = Buffer.from(JSON.stringify(value))
+
+  return new Promise((resolve, reject) => {
+    ch.publish(
+      exchange,
+      routingKey,
+      content,
+      { contentType: 'application/json' },
+      (err) => {
+        if (err !== null) {
+          reject(new Error('Message was NACKed by the broker'))
+        } else {
+          resolve()
+        }
+      },
+    )
+  })
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,22 @@
 import amqp from 'amqplib'
+import type { PlayingState } from '../internal/gamelogic/gamestate'
+import { publishJSON } from '../internal/pubsub/publish'
+import { ExchangePerilDirect, PauseKey } from '../internal/routing/routing'
 
 async function main() {
   const rabbitConnString = 'amqp://guest:guest@localhost:5672/'
   const conn = await amqp.connect(rabbitConnString)
   console.log('Peril game server connected to RabbitMQ!')
+
+  const publishCh = await conn.createConfirmChannel()
+
+  try {
+    await publishJSON<PlayingState>(publishCh, ExchangePerilDirect, PauseKey, {
+      isPaused: true,
+    })
+  } catch (err) {
+    console.error('Error publishing message:', err)
+  }
 
   const signals = ['SIGINT', 'SIGTERM']
   signals.forEach((signal) => {
